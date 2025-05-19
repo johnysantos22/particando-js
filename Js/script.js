abd3c9783a43dcac8dd55492916fb90c24f96c29
johnysantos22 <santosjohny302@gmail.com>
2025-05-19 10:19:54 -0300

    praticando logica de programação

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+
+const soma = prompt("digite um numero");
+const soma2 = prompt("digite outro numero");
+
+ const resultado = Number(soma) + Number(soma2);
+
+alert(`o resultado da soma entre ${soma} + ${soma2} é = ${resultado}`);
