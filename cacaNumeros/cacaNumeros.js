758670cccc0603a23f5fc2c9e6212b53978342a3
johnysantos22 <santosjohny302@gmail.com>
2025-05-20 11:18:44 -0300

    praticando minha logica com FOR

---
+++ cacaNumeros/cacaNumeros.js
@@ -0,0 +1,15 @@
+
+function cacaNumeros(_a, b, c) {
+    let resultado = 0
+
+    for (let i = a; i <= b; i++) {
+        if (a % c === 0 ) {
+         resultado [i]
+        } 
+    }
+    return resultado;
+
+}
+
+console.log(cacaNumeros(3, 10, 2));
+
