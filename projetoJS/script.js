8b3c0a922e001641e0454baf38103f7a0f714169
johnysantos22 <santosjohny302@gmail.com>
2025-05-23 14:17:13 -0300

    comcluindo projeto de lista de item / saparando as pastas

--- projetoJS/script.js
+++ projetoJS/script.js
@@ -2,13 +2,26 @@
 const botao = document.getElementById('addItemBtn');
 botao.addEventListener('click', clicarBotao)
 
+let total = 0;
+
 function clicarBotao() {
+    const itemName = document.getElementById('itemName').value;
+    const itemPriceStr = document.getElementById('itemPrice').value;
+    const itemPrice = parseFloat(itemPriceStr);
 
-const itemName = document.getElementById('itemName').value;
-const itemPrice = document.getElementById('itemPrice').value;
+    if (itemName.trim() !== '' && !isNaN(itemPrice) && itemPrice >= 0) {
+        const lista = document.getElementById('itemList')
+        const novoItem = document.createElement('li');
+        novoItem.textContent = itemName + ' - R$ ' + itemPrice.toFixed(2);
+        lista.appendChild(novoItem);
 
+        total += itemPrice;
+        document.getElementById('totalAmount').textContent = 'Total: R$ ' + total.toFixed(2);
 
- document.getElementById('itemList').textContent = itemName;
- document.getElementById('totalAmount').textContent = itemPrice;
-
+        document.getElementById('itemName').value = '';
+        document.getElementById('itemPrice').value = '';
+    } else {
+        alert('Por favor, digite algo antes de adicionar');
+    }
 }
+
